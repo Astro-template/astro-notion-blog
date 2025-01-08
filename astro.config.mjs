93a17eea445ef9ff882b7b8ecedcccadea79320a
aihub <ouraihub@gmail.com>
2025-01-08 22:21:43 +0800

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,15 +10,10 @@ const getSite = function () {
     return new URL(BASE_PATH, `https://${CUSTOM_DOMAIN}`).toString();
   }
 
-  if (process.env.VERCEL && process.env.VERCEL_URL) {
-    return new URL(BASE_PATH, `https://${process.env.VERCEL_URL}`).toString();
-  }
-
   if (process.env.CF_PAGES) {
     if (process.env.CF_PAGES_BRANCH !== 'main') {
       return new URL(BASE_PATH, process.env.CF_PAGES_URL).toString();
     }
-
     return new URL(
       BASE_PATH,
       `https://${new URL(process.env.CF_PAGES_URL).host
@@ -33,7 +28,7 @@ const getSite = function () {
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://1staihub.github.io',
+  site: getSite(),
   base: '/',
   integrations: [
     CoverImageDownloader(),
