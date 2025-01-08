0e5bf6fcbda437b26a90f90b09252adecc77ba1d
aihub <ouraihub@gmail.com>
2025-01-08 23:25:29 +0800

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,35 +10,22 @@ const getSite = function () {
     return new URL(BASE_PATH, `https://${CUSTOM_DOMAIN}`).toString();
   }
 
-  if (process.env.VERCEL && process.env.VERCEL_URL) {
-    return new URL(BASE_PATH, `https://${process.env.VERCEL_URL}`).toString();
-  }
-
   if (process.env.CF_PAGES) {
-    if (process.env.CF_PAGES_BRANCH !== 'main') {
-      return new URL(BASE_PATH, process.env.CF_PAGES_URL).toString();
-    }
-
-    return new URL(
-      BASE_PATH,
-      `https://${new URL(process.env.CF_PAGES_URL).host
-        .split('.')
-        .slice(1)
-        .join('.')}`
-    ).toString();
+    return process.env.SITE_URL || 'https://astro-notion-blog.pages.dev';
   }
 
-  return new URL(BASE_PATH, 'http://localhost:4321').toString();
+  return 'http://localhost:4321';
 };
 
 // https://astro.build/config
 export default defineConfig({
   site: getSite(),
-  base: BASE_PATH,
+  base: '/',
   integrations: [
     CoverImageDownloader(),
     CustomIconDownloader(),
     FeaturedImageDownloader(),
     PublicNotionCopier(),
   ],
+  output: 'static'
 });
