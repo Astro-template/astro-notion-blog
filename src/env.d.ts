c7c4237f2e627c15b5cbcd6f07828bd63b854a8f
aihub <ouraihub@gmail.com>
2025-01-09 00:23:12 +0800

    first commit

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="astro/client" />
+/// <reference types="@astrojs/image/client" />
+
+interface ImportMetaEnv {
+  readonly NOTION_API_SECRET: string;
+  readonly DATABASE_ID: string;
+  readonly SITE_URL: string;
+  readonly CUSTOM_DOMAIN: string;
+  readonly BASE_PATH: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
